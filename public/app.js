bd6f94d20721d8ff920143b3a6703009fca408af
Geoff Symonds <gsymonds@paychex.com>
2023-10-16 16:42:31 -0400

    data binding part 1

--- public/app.js
+++ public/app.js
@@ -1,5 +1,7 @@
+// Module
 var weatherApp = angular.module("weatherApp", ["ngRoute", "ngResource"]);
 
+// Routes
 weatherApp.config(function ($routeProvider) {
     $routeProvider
         .when("/", {
@@ -9,13 +11,19 @@ weatherApp.config(function ($routeProvider) {
         .when("/forecast", {
             templateUrl: "pages/forecast.htm",
             controller: "forecastController"
+        })
+        .when("/forecast/:days", {
+            templateUrl: "pages/forecast.htm",
+            controller: "forecastController"
         });
 });
 
+// Custom Service
 weatherApp.service("cityService", function () {
     this.city = "Rochester, NY";
 });
 
+// Controllers
 weatherApp.controller("homeController", [
     "$scope",
     "cityService",
@@ -30,8 +38,23 @@ weatherApp.controller("homeController", [
 
 weatherApp.controller("forecastController", [
     "$scope",
+    "$resource",
+    "$routeParams",
     "cityService",
-    function ($scope, cityService) {
+    function ($scope, $resource, $routeParams, cityService) {
         $scope.city = cityService.city;
+
+        $scope.days = $routeParams.days || 5;
+
+        $scope.weatherAPI = $resource(
+            "http://api.weatherapi.com/v1/forecast.json?key=65c096c37d0b483c9a9184556231610",
+            { callback: "JSON_CALLBACK" },
+            +"&" + { get: { method: "JSONP" } }
+        );
+
+        $scope.weatherResult = $scope.weatherAPI.get({
+            q: $scope.city,
+            days: $scope.days
+        });
     }
 ]);
