09d24cc72f646976924140f48c7009042278c322
Geoff Symonds <gsymonds@paychex.com>
2023-10-16 14:25:23 -0400

    add city service

--- public/app.js
+++ public/app.js
@@ -12,6 +12,26 @@ weatherApp.config(function ($routeProvider) {
         });
 });
 
-weatherApp.controller("homeController", ["$scope", function ($scope) {}]);
+weatherApp.service("cityService", function () {
+    this.city = "Rochester, NY";
+});
 
-weatherApp.controller("forecastController", ["$scope", function ($scope) {}]);
+weatherApp.controller("homeController", [
+    "$scope",
+    "cityService",
+    function ($scope, cityService) {
+        $scope.city = cityService.city;
+
+        $scope.$watch("city", function () {
+            cityService.city = $scope.city;
+        });
+    }
+]);
+
+weatherApp.controller("forecastController", [
+    "$scope",
+    "cityService",
+    function ($scope, cityService) {
+        $scope.city = cityService.city;
+    }
+]);
