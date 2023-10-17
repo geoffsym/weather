957d7ed0cdcaffc395f7effe0f97579416ed4f3c
Geoff Symonds <gsymonds@paychex.com>
2023-10-17 10:21:23 -0400

    ng-submit

--- public/controllers.js
+++ public/controllers.js
@@ -1,13 +1,18 @@
 // Controllers
 weatherApp.controller("homeController", [
     "$scope",
+    "$location",
     "cityService",
-    function ($scope, cityService) {
+    function ($scope, $location, cityService) {
         $scope.city = cityService.city;
 
         $scope.$watch("city", function () {
             cityService.city = $scope.city;
         });
+
+        $scope.submit = function () {
+            $location.path("/forecast");
+        };
     }
 ]);
 
