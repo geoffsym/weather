5a59a77b6c2fe22ed6a7a3b1b3934e1f677f0650
Geoff Symonds <gsymonds@paychex.com>
2023-10-17 10:03:55 -0400

    custom directive

--- public/app.js
+++ public/app.js
@@ -58,3 +58,16 @@ weatherApp.controller("forecastController", [
         });
     }
 ]);
+
+// Directives
+weatherApp.directive("weatherReport", function () {
+    return {
+        restrict: "E",
+        templateUrl: "directives/weatherReport.html",
+        replace: false,
+        scope: {
+            weatherDay: "=",
+            dateFormat: "@"
+        }
+    };
+});
